cd17f93d9a926b598adcf63e6b24a781b5e550fd
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-05-01 09:00:21 +0530

    Added shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       Sandeep Sahu - Full Stack Developer
+      <Button>Click me</Button>
     </main>
   );
 }
