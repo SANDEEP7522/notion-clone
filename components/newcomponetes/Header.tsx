f58efb0c0c14da9984ed5b8c23d0432a2b39fa73
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-05-02 19:16:04 +0530

    Added Heder with Authentication

---
+++ components/newcomponetes/Header.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+
+const Header = () => {
+  const { user } = useUser();
+  return (
+    <div className="flex justify-between items-center">
+      {user && (
+        <h1 className="text-3xl font-bold">
+          {user?.firstName}
+          {`'`}s Notation
+        </h1>
+      )}
+      <div className="flex items-center gap-x-5">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
